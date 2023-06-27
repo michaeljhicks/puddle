3a985fd6403729a0c780dbc14057b9c95a8bb933
Michael Hicks <hicks.michael04@gmail.com>
2023-06-27 15:40:28 -0600

    Feat: user signup

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from item.models import Category, Item
+from .forms import SignUpForm
 
 # Create your views here.
 def index(request):
@@ -13,3 +14,10 @@ def index(request):
 
 def contact(request):
     return render(request, 'core/contact.html')
+
+def signup(request):
+    form = SignUpForm()
+
+    return render(request, 'core/signup.html', {
+        'form': form,
+    })
